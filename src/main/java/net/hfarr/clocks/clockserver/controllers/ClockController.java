183ddaf1aa600a6d478b7fbc07bc6f1e79fbda1c
Henry Farr <hfarr@hey.com>
2023-10-16 21:13:22 -0700

    Fix missing web dependency

--- src/main/java/net/hfarr/clocks/clockserver/controllers/ClockController.java
+++ src/main/java/net/hfarr/clocks/clockserver/controllers/ClockController.java
@@ -3,11 +3,8 @@ package net.hfarr.clocks.clockserver.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-// @RestCon
 @RestController
-
 public class ClockController {
-
 
   @GetMapping(path = "/clock")
   public String getClock() {
