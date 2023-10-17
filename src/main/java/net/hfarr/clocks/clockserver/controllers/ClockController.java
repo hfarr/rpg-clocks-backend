b3e776a6f163ab84e895bc249d424ce697c189ee
Henry Farr <hfarr@hey.com>
2023-10-16 21:20:23 -0700

    Return a clock from CLockController

--- src/main/java/net/hfarr/clocks/clockserver/controllers/ClockController.java
+++ src/main/java/net/hfarr/clocks/clockserver/controllers/ClockController.java
@@ -3,13 +3,27 @@ package net.hfarr.clocks.clockserver.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+import net.hfarr.clocks.clockserver.model.ClockModel;
+
+@Slf4j
 @RestController
 public class ClockController {
 
   @GetMapping(path = "/clock")
-  public String getClock() {
-    return "Hello, world!";
+  public ClockModel getClock() {
+
+    log.info("Request received: /clock");
+    final ClockModel result = ClockModel.builder()
+      .segments(8)
+      .progress(3)
+      .build();
+    
+
+    return result;
   }
+
+  
   
 }
   
--- src/main/java/net/hfarr/clocks/clockserver/model/ClockModel.java
+++ src/main/java/net/hfarr/clocks/clockserver/model/ClockModel.java
@@ -1,8 +1,10 @@
 package net.hfarr.clocks.clockserver.model;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class ClockModel {
 
   private String name;
