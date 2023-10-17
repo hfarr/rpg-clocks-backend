7ad210b530372ec9cefde849a90dd0d97d58cc97
Henry Farr <hfarr@hey.com>
2023-10-16 21:41:33 -0700

    Enable CrossOrigin requests on ClockController

--- src/main/java/net/hfarr/clocks/clockserver/controllers/ClockController.java
+++ src/main/java/net/hfarr/clocks/clockserver/controllers/ClockController.java
@@ -1,5 +1,6 @@
 package net.hfarr.clocks.clockserver.controllers;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import net.hfarr.clocks.clockserver.model.ClockModel;
 
 @Slf4j
+@CrossOrigin
 @RestController
 public class ClockController {
 
