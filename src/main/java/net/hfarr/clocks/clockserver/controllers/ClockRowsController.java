608beeace3f995b7b15fb645ee5a058ba07aac65
Henry Farr <hfarr@hey.com>
2023-10-24 19:57:26 -0700

    add validation to add clock route

--- src/main/java/net/hfarr/clocks/clockserver/controllers/ClockRowsController.java
+++ src/main/java/net/hfarr/clocks/clockserver/controllers/ClockRowsController.java
@@ -2,7 +2,9 @@ package net.hfarr.clocks.clockserver.controllers;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -32,23 +34,33 @@ public class ClockRowsController {
   }
 
   @PostMapping("/{id}/add")
-  public void addClock(@RequestParam Integer id, @RequestBody ClockModel clock) {
+  public ResponseEntity<String> addClock(@RequestParam Integer id, @RequestBody ClockModel clock) {
+    if ( id >= globalRows.size() || id < 0 ) {
+      return ResponseEntity.badRequest().body("Row does not exist.");
+    }
 
     ClockRowModel appendee = globalRows.get(id);
 
     appendee.addClock(clock);
 
     // TODO send SSE
+    return ResponseEntity.ok().build();
   }
 
   @PostMapping("/add")
   public void addRow(@RequestBody ClockModel clock) {
+
     globalRows.add(new ClockRowModel());
     // TODO send SSE
   }
 
   @PutMapping("/set")
   public void setRows(@RequestBody List<ClockRowModel> newRows) {
+    if ( Objects.isNull(newRows) ) {
+      // Is this possible?
+      return;
+    }
+
     this.globalRows = newRows;
 
     // TODO send SSE
