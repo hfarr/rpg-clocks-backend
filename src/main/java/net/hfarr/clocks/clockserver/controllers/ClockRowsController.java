8e8355776e5f3861e05be7f0026eb54d795afc28
Henry Farr <hfarr@hey.com>
2023-11-05 08:36:44 -0800

    cleanup

--- src/main/java/net/hfarr/clocks/clockserver/controllers/ClockRowsController.java
+++ src/main/java/net/hfarr/clocks/clockserver/controllers/ClockRowsController.java
@@ -1,12 +1,9 @@
 package net.hfarr.clocks.clockserver.controllers;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.Scanner;
@@ -20,13 +17,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
 
-import jakarta.annotation.PreDestroy;
 import lombok.extern.slf4j.Slf4j;
 import net.hfarr.clocks.clockserver.model.ClockModel;
 import net.hfarr.clocks.clockserver.model.ClockRowModel;
@@ -42,9 +36,7 @@ public class ClockRowsController {
   private final SseTable tableEventGenerator; // TODO do I like this name
 
   private final File database;
-  private BufferedWriter dbWriter;
   private ObjectMapper objectMapper;
-  private ObjectWriter objWriter;
 
   @Autowired
   public ClockRowsController(SseTable tableEventEmitter) {
