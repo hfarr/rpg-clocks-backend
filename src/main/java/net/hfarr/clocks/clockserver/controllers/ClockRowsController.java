d6a3d975e60a54b0cc8330a6f9158a1c4ea7afde
Henry Farr <hfarr@hey.com>
2023-11-05 08:32:50 -0800

    Add reading in from file

--- src/main/java/net/hfarr/clocks/clockserver/controllers/ClockRowsController.java
+++ src/main/java/net/hfarr/clocks/clockserver/controllers/ClockRowsController.java
@@ -1,8 +1,15 @@
 package net.hfarr.clocks.clockserver.controllers;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.Scanner;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +23,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+
+import jakarta.annotation.PreDestroy;
 import lombok.extern.slf4j.Slf4j;
 import net.hfarr.clocks.clockserver.model.ClockModel;
 import net.hfarr.clocks.clockserver.model.ClockRowModel;
@@ -30,25 +41,47 @@ public class ClockRowsController {
   private List<ClockRowModel> globalRows;
   private final SseTable tableEventGenerator; // TODO do I like this name
 
+  private final File database;
+  private BufferedWriter dbWriter;
+  private ObjectMapper objectMapper;
+  private ObjectWriter objWriter;
+
   @Autowired
   public ClockRowsController(SseTable tableEventEmitter) {
 
-    // TODO load from file
-    globalRows = new ArrayList<ClockRowModel>();
-    globalRows.add(ClockRowModel.createEmptyRow("Row 1"));
-    globalRows.add(ClockRowModel.createEmptyRow("Row 2"));
-    globalRows.get(0).addClock(ClockModel.builder()
-        .segments(8)
-        .progress(2)
-        .build());
-    globalRows.get(0).addClock(ClockModel.builder()
-        .segments(6)
-        .progress(2)
-        .build());
-    
     tableEventGenerator = tableEventEmitter;
+    database = new File("data/clocks.json");
+
+    try {
+      loadFromFile();
+    } catch (IOException ioe) {
+      log.error("Failed to load from file {}", ioe.getMessage());
+    }
 
     log.info("initialized");
+
+  }
+
+  // Roughshod persistence. Slipknick. Lickspittle.
+  private void loadFromFile() throws IOException {
+    
+    log.info("File path {}", database.getAbsolutePath());
+    objectMapper = new ObjectMapper();
+
+    if (!database.exists()) {
+
+      database.createNewFile();
+
+      File seedFile = new File("data/seed-clocks.json");
+      Scanner sc = new Scanner(seedFile);
+      BufferedWriter bw = new BufferedWriter(new FileWriter(database));
+      bw.write(sc.nextLine());
+      sc.close();
+      bw.close();
+
+    }
+
+    globalRows = List.of( objectMapper.readValue(database, ClockRowModel[].class ) );
 
   }
 
--- src/main/java/net/hfarr/clocks/clockserver/model/ClockModel.java
+++ src/main/java/net/hfarr/clocks/clockserver/model/ClockModel.java
@@ -3,10 +3,12 @@ package net.hfarr.clocks.clockserver.model;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class ClockModel {
 
   private String name;
